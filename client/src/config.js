862209766c31f5408e58d6e20d5bcaddfc9bc7e9
techshack103 <161464701+techshack103@users.noreply.github.com>
2024-03-03 20:27:11 +0530

    api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: process.env.REACT_APP_API_URL,
+  apiUrl: "http://51.20.3.206/scrape/devops" || process.env.REACT_APP_API_URL,
 };
 console.log("API URL: ", this.apiUrl)
 module.exports = config;
